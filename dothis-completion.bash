06b24b7278da9587d66270bd66ecb2f9d74077b3
Lazarus Lazaridis <lazarus.lazaridis@gmail.com>
2018-03-01 02:42:10 +0200

    Upload code.

---
+++ dothis-completion.bash
@@ -0,0 +1,47 @@
+#/usr/bin/env bash
+#
+# MIT License
+#
+# Copyright (c) 2018 Lazarus Lazaridis
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+_dothis_completions()
+{
+  if [ "${#COMP_WORDS[@]}" != "2" ]; then
+    return
+  fi
+
+  local commands_number=${DOTHIS_COMPLETION_COMMANDS_NUMBER:-50}
+  local IFS=$'\n'
+  local suggestions=($(compgen -W "$(fc -l -$commands_number | sed 's/\t//')" -- "${COMP_WORDS[1]}"))
+
+  if [ "${#suggestions[@]}" == "1" ]; then
+    local number=$(echo ${suggestions[0]/%\ */})
+    COMPREPLY=("$number")
+  else
+    for i in "${!suggestions[@]}"; do
+      suggestions[$i]="$(printf '%*s' "-$COLUMNS"  "${suggestions[$i]}")"
+    done
+
+    COMPREPLY=("${suggestions[@]}")
+  fi
+}
+
+complete -F _dothis_completions dothis
--- dothis.bash
+++ dothis.bash
@@ -22,4 +22,16 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-"$@"
+if [ -z "$1" ]; then
+  echo "No command number passed"
+  exit 2
+fi
+
+exists=$(fc -l -1000 | grep ^$1 -- 2>/dev/null)
+
+if [ -n "$exists" ]; then
+  fc -s -- "$1"
+else
+  echo "Command with number $1 was not found in recent history"
+  exit 2
+fi
